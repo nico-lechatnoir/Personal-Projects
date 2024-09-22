5bd0b7558f6075d83b6dbbb92fdb85e17d99bbc7
Nico <144506187+nico-lechatnoir@users.noreply.github.com>
2024-09-22 17:44:15 -0400

    Cleaned and updated

--- Tarot Note Generator.py
+++ Tarot Note Generator.py
@@ -1,5 +1,4 @@
 import os
-from pprint import pp
 
 full_path = os.path.realpath(__file__)
 localDir = os.path.dirname(full_path)
@@ -66,14 +65,6 @@ LIMIT 10
 ```
 """
 
-def create_list():
-    for s in SUITS:
-        suit_list = []
-        for n in NUMBERS:
-            suit_list.append(n +" of " + s)
-        MINOR.append(suit_list)
-    print(MINOR)
-
 def create_dic():
     for s in SUITS:
         suit_dic = {}
@@ -82,32 +73,6 @@ def create_dic():
             suit_dic[num] = n +" of " + s
             num+=1
         MINOR[s] = suit_dic
-
-def create_major_card():
-    for i in range(len(MAJOR)):
-        f_name = MAJOR.get(i) + ".md"
-        newFile = os.path.join(tPath, f_name)
-        num = 1
-        with open(newFile, 'w+') as fp:
-            fp.write(CARD.format(card = MAJOR.get(i), ord = num, arcana = "Major", suit="", tag=""))
-            num += 1
-            return "One Card Made"
-    ##return "Major Cards Created"
-
-def create_minor_card():
-    for s in range(len(SUITS)):
-        num = 1
-        for n in range(len(NUMBERS)):
-            f_name = MINOR[s][n] + ".md"
-            newFile = os.path.join(tPath, f_name)
-            with open(newFile, 'w+') as fp:
-                fp.write(CARD.format(card = MINOR[s][n], ord = num,  arcana = "Minor", suit = SUITS[s], tag = "/" + SUITS[i]))
-                num += 1
-    return "Minor Cards Created"
-
-#create_major_card()
-##create_minor_card()
-
 
 def create_all_cards():
     create_dic()
@@ -122,4 +87,5 @@ def create_all_cards():
         newFile = os.path.join(tPath, f_name)
         with open(newFile, 'w+') as fp:
             fp.write(CARD.format(card = card, ord = ord+1, arcana = "Major", suit="", tag=""))
+
 create_all_cards()
